187fb2847575931b9381d8266607db3584165278
ETA444 <eta444@pm.me>
2026-05-02 11:19:15 +0200

    Initial snapshot of cryptopandemic.com public_html
    - Add full site source synced from cPanel via cpanel-sync.sh
    - Includes all current pages, assets, and configurations
    - Baseline for version-controlled iteration going forward
    
    Synced from: premium85.web-hosting.com:/home/georznmu/cryptopandemic.com

---
+++ public_html/ncsitebuilder/a188dd9cfa60000804666376dffe914b.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<script type="text/javascript">
+			</script>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars((isset($seoTitle) && $seoTitle !== "") ? $seoTitle : "CryptoPandemic's Official Website"); ?></title>
+	<base href="{{base_url}}" />
+	<?php echo isset($sitemapUrls) ? (generateCanonicalUrl($sitemapUrls)."\n") : ""; ?>	
+	
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+						<meta name="keywords" content="<?php echo htmlspecialchars((isset($seoKeywords) && $seoKeywords !== "") ? $seoKeywords : "crypto,pandemic,cryptopandemic,george dreemer,datasafari,skipschoolmakemoney,bitcoin,ethereum,crypto news,crypto blog,crypto reddit,dreemcorp"); ?>" />
+			
+	<!-- Facebook Open Graph -->
+						<!-- Facebook Open Graph end -->
+
+		<meta name="generator" content="Website Builder" />
+			<script src="js/common-bundle.js?ts=20260430192201" type="text/javascript"></script>
+	<script src="js/a188dd9cfa60000804666376dffe914b-bundle.js?ts=20260430192201" type="text/javascript"></script>
+	<link href="css/common-bundle.css?ts=20260430192201" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin,latin-ext,vietnamese" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin,latin-ext,vietnamese" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=devanagari,latin,latin-ext" rel="stylesheet" type="text/css" />
+	<link href="css/a188dd9cfa60000804666376dffe914b-bundle.css?ts=20260430192201" rel="stylesheet" type="text/css" id="wb-page-stylesheet" />
+	<ga-code/><!-- HTML Meta Tags -->
+<title>CryptoPandemic Official Website</title>
+<meta name="description" content="Welcome to CryptoPandemic's Official Website!">
+<meta name="author" content="George Dreemer">
+<meta name="robots" content="index, follow">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://cryptopandemic.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="CryptoPandemic's Official Website">
+<meta property="og:description" content="Welcome to CryptoPandemic's Official Website!">
+<meta property="og:image" content="https://cryptopandemic.com/gallery_gen/e6752594e756a7de0a81a328e4fdf98a_fit.png">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="cryptopandemic.com">
+<meta property="twitter:url" content="https://cryptopandemic.com">
+<meta name="twitter:title" content="CryptoPandemic's Official Website">
+<meta name="twitter:description" content="Welcome to CryptoPandemic's Official Website!">
+<meta name="twitter:image" content="https://cryptopandemic.com/gallery_gen/e6752594e756a7de0a81a328e4fdf98a_fit.png"><link rel="shortcut icon" href="gallery/favicons/favicon.ico" type="image/x-icon">
+	<script type="text/javascript">
+	window.useTrailingSlashes = false;
+	window.disableRightClick = false;
+	window.currLang = 'en';
+</script>
+	<script src="https://www.google.com/recaptcha/enterprise.js?render=6Ld1cxgqAAAAAOVKHfyBhh2x9LJjmfHxQQbapcBc"></script>	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+		<script type="text/javascript">
+		$(function () {
+<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success"); ?>
+<?php if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
+	<?php if (($wb_form_popup_mode = popSessionOrGlobalVar("wb_form_popup_mode")) && (isset($wbPopupMode) && $wbPopupMode)) { ?>
+		if (window !== window.parent && window.parent.postMessage) {
+			var data = {
+				event: "wb_contact_form_sent",
+				data: {
+					state: "<?php echo str_replace('"', '\"', $wb_form_send_state); ?>",
+					type: "<?php echo $wb_form_send_success ? "success" : "danger"; ?>"
+				}
+			};
+			window.parent.postMessage(data, "<?php echo str_replace('"', '\"', popSessionOrGlobalVar("wb_target_origin")); ?>");
+		}
+	<?php $wb_form_send_success = false; $wb_form_send_state = null; $wb_form_popup_mode = false; ?>
+	<?php } else { ?>
+		wb_show_alert("<?php echo str_replace(array('"', "\r", "\n"), array('\"', "", "<br/>"), $wb_form_send_state); ?>", "<?php echo $wb_form_send_success ? "success" : "danger"; ?>");
+	<?php } ?>
+<?php } ?>
+});    </script>
+</head>
+
+
+<body class="site site-lang-en<?php if (isset($wbPopupMode) && $wbPopupMode) echo ' popup-mode'; ?> " <?php ?>><div id="wb_root" class="root wb-layout-vertical"><div class="wb_sbg"></div><div id="wb_header_a188dd9cfa60000804666376dffe914b" class="wb_element wb-sticky wb-layout-element" data-plugin="LayoutElement" data-h-align="center" data-v-align="top"><div class="wb_content wb-layout-vertical"><div id="a190eac54197008035e9335e6990b156" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a190eace4c340046607af6d3ceecb501" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><img loading="lazy" alt="" src="gallery_gen/36043f11bd40c991d924a62911ddccd4_222x222_fit.png?ts=1777566121"></div></div></div><div id="a190eac541a4006511594596398175ae" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a190eac541a7003d6c9988274fb9b352" class="wb_element wb-menu wb-prevent-layout-click wb-anim wb-anim-move-up" data-plugin="Menu"><?php MenuElement::render((object) array(
+	'type' => 'hmenu',
+	'dir' => 'ltr',
+	'items' => array(
+		(object) array(
+			'id' => 1,
+			'href' => '#contact',
+			'name' => 'Contact',
+			'iconAlign' => 'left',
+			'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="1793.982" height="1793.982" viewBox="0 0 1793.982 1793.982" style="display: inline-block; vertical-align: middle; position: relative; top: -1px; height: 1em; width: 1em; overflow: visible; direction: ltr;"><text x="1.501415" y="1537.02" font-size="1792" fill="currentColor" style="font-family: &quot;FontAwesome&quot;"></text></svg>',
+			'anchor' => 'contact',
+			'class' => 'wb_this_page_menu_item active',
+			'children' => array()
+		)
+	)
+)); ?><div class="clearfix"></div></div></div></div></div></div></div></div><div id="wb_main_a188dd9cfa60000804666376dffe914b" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a188dd9ceaf9062e31cd48cbb6ca84ba" class="wb_element wb-layout-element" data-plugin="LayoutElement"><a name="home" class="wb_anchor"></a><div class="wb_content wb-layout-horizontal"><div id="a188dd9ceaf9062e31cd48cbb6ca84ba_video_background" class="wb_video_background" data-video="gallery/Outro-Landscape-NFAA-FrontPage_compressed1 - COMPRESS.mp4" data-start="1" data-end=""></div></div></div><div id="a190eb4499c400e12dd9befad032b5d0" class="wb_element wb-elm-orient-horizontal" data-plugin="Line"><div class="wb-elm-line"></div></div><div id="a188dd9ceaf90c3d52908e3e83685b12" class="wb_element wb-anim-entry wb-anim wb-anim-fade-in-none wb-layout-element" data-plugin="LayoutElement" data-wb-anim-entry-time="0.6" data-wb-anim-entry-delay="0"><div class="wb_content wb-layout-vertical"><div id="a19ddf20688200f9db07227430bd0198" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h4 class="wb-stl-pagetitle" style="text-align: center;">Viral-by-Design. Marketing &amp; Analytics for Web3.</h4>
+</div><div id="a188dd9ceaf90ef70de43eda6b93739e" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;">CryptoPandemic engineers contagious Web3 experiences. From brand identity to on-chain strategy. We don't help projects grow. We make them spread.</p>
+</div><div id="a19dd9afa1c6007d7b9f2828ad0b5777" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a19dd9b4b97f0089d52b746cb39ed058" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a19ddec339600065d73f3ff88b8a6995" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><img loading="lazy" alt="" src="gallery_gen/5f3138619308e1550ce135d4a0a5e215_344x344_fit.png?ts=1777566122"></div></div></div><div id="a19ddf1bad7b00f0f6db2cbfca0f5ea0" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><img loading="lazy" alt="" src="gallery_gen/5f3138619308e1550ce135d4a0a5e215_360x360_fit.png?ts=1777566122"></div></div></div><div id="a19dd9afa1d800c9fa5c0fbcbebdfa47" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a19dd9afa1da00c16ec54e57e94051b9" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">Marketing &amp; Design</h5>
+</div><div id="a19ddf02b94d0022027255887fa13593" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h3 class="wb-stl-heading3" style="text-align: center;"><em>Strategy and content at Web3 speed.</em></h3>
+</div><div id="a19dd9afa1dc008da616f59bff1244f5" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#ffffff">We build full-stack marketing systems from strategy to design to content. All in-house. Every asset is created with the goal of turning attention into community.</font></p>
+</div></div></div></div></div><div id="a19ddf0a0d8600199de0b96b1447ce6e" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a19ddf0a0d890031cc7942df7a2f0f6a" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><img loading="lazy" alt="" src="gallery_gen/0a945a6cd75aee51e2a3b3ec1f1b1db7_344x344_fit.png?ts=1777566122"></div></div></div><div id="a19ddf1cbe21004de74ef948a7e0f60f" class="wb_element wb_element_picture" data-plugin="Picture" title=""><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><img loading="lazy" alt="" src="gallery_gen/0a945a6cd75aee51e2a3b3ec1f1b1db7_360x360_fit.png?ts=1777566122"></div></div></div><div id="a19ddf0a0d8e00be96bde12652c2020e" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a19ddf0a0d8f001b51347d7187f9c2c2" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h5 class="wb-stl-subtitle" style="text-align: center;">Analytics &amp; Engineering</h5>
+</div><div id="a19ddf0a0d920041d5785601efaaa104" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h3 class="wb-stl-heading3" style="text-align: center;"><i>Data-driven from start to finish.</i></h3>
+</div><div id="a19ddf0a0d940070babd7cce320a2fe2" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#ffffff">Data is the backbone of Web3, from social media to on-chain analytics, all engineered to turn raw data into decisions and technology into utility.</font></p>
+</div></div></div></div></div></div></div></div></div><div id="a19dd9813a8c00e1a6a471d450cb09a2" class="wb_element wb-anim-entry wb-anim wb-anim-fade-in-none wb-layout-element" data-plugin="LayoutElement" data-wb-anim-entry-time="0.6" data-wb-anim-entry-delay="0"><div class="wb_content wb-layout-vertical"><div id="a19ddf1a488c00e37fdbd96e23cd5c27" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h4 class="wb-stl-pagetitle" style="text-align: center;">Contact us</h4>
+</div><div id="a19dd9813a94008b67ca4be05e2db0c8" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-normal" style="text-align: center;"><font color="#ffffff">To contact us, fill out the form below.</font></p>
+</div><div id="a19dd9813a9800c456a618a8d34c1cf9" class="wb_element" data-plugin="Form"><form id="a19dd9813a9800c456a618a8d34c1cf9_form" class="wb_form wb_vertical_form wb_placeholder_form wb_form_ltr wb_form_vertical" method="post" enctype="multipart/form-data" action="__wb_curr_url__"><input type="hidden" name="wb_form_id" value="91967b35"><input type="hidden" name="wb_form_uuid" value="2662994c"><input type="hidden" name="secure_token" value="<?php echo session_id() ? ('a19dd9813a9800c456a618a8d34c1cf9:' . ($_SESSION['wb_form_secure_token_a19dd9813a9800c456a618a8d34c1cf9'] = md5(microtime()."a188dd9cfa60000804666376dffe914b"))) : ''?>"><textarea name="message" rows="3" cols="20" class="hpc" autocomplete="off"></textarea><table><tr><th></th><td><input type="hidden" name="wb_input_0" value="Name"><div><input class="form-control form-field" type="text" value="" placeholder="Name" maxlength="255" name="wb_input_0" required="required"></div></td></tr><tr><th></th><td><input type="hidden" name="wb_input_1" value="Email"><div><input class="form-control form-field" type="email" value="" placeholder="Email" name="wb_input_1" required="required"></div></td></tr><tr class="area-row"><th></th><td><input type="hidden" name="wb_input_2" value="Message"><div><textarea class="form-control form-field form-area-field" rows="4" placeholder="Message" name="wb_input_2" required="required"></textarea></div></td></tr><tr class="form-footer"><td colspan="2" class="text-right"><button type="submit" class="btn btn-default"><span>Send</span></button></td></tr></table><?php if (isset($wbPopupMode) && $wbPopupMode): ?><input type="hidden" name="wb_popup_mode" value="1" /><?php endif; ?></form><script type="text/javascript">
+			<?php $wb_form_id = sessionOrGlobalVar("wb_form_id"); if ($wb_form_id == "91967b35") { ?>
+				<?php popSessionOrGlobalVar("wb_form_id"); ?>
+				var formValues = <?php echo json_encode(popSessionOrGlobalVar("post")); ?>;
+				var formErrors = <?php echo json_encode(popSessionOrGlobalVar("formErrors")); ?>;
+				wb_form_validateForm("91967b35", formValues, formErrors);
+			<?php } ?>
+			</script><script>$(function () {
+	$('#a19dd9813a9800c456a618a8d34c1cf9 form').on('submit', function (e) {
+		if (document.cookieIsAllowed && !document.cookieIsAllowed("_GRECAPTCHA")) {
+			e.stopPropagation();
+			$(this).find('button[type=submit]').append($('<input>').attr('type', 'hidden').attr('name', 'cookieDontAllow').val('1'));
+			return true;
+		}		;return true;
+	});
+});
+</script></div></div></div></div></div><div id="wb_footer_a188dd9cfa60000804666376dffe914b" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a190eaf64b430022b1067000ebd6f314" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a190eb20e7490072e382494593088cc3" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-footer" style="text-align: center;"><strong><span style="background-color:rgba(6, 6, 6, 0.66);"><span style="color:#866cba;">+ Related Domains +</span></span></strong></p>
+
+<p class="wb-stl-footer" style="text-align: center;"><a data-_="Link" href="https://www.datasafari.dev" target="_blank" title="DataSafari Official Website">datasafari.dev</a></p>
+
+<p class="wb-stl-footer" style="text-align: center;"><a data-_="Link" href="https://www.georgedreemer.com" target="_blank" title="George Dreemer Official Website">georgedreemer.com</a></p>
+
+<p class="wb-stl-footer" style="text-align: center;"><a data-_="Link" href="https://www.skipschoolmakemoney.com" target="_blank" title="SkipSchoolMakeMoney Official Website">skipschoolmakemoney.com</a></p>
+
+<p class="wb-stl-footer" style="text-align: center;"><a data-_="Link" href="https://www.dreemcorp.com" target="_blank" title="DREEMCORP Official Website">dreemcorp.com</a></p>
+
+<p class="wb-stl-footer" style="text-align: center;"><a data-_="Link" href="anabolicmusick.com" target="_blank" title="Anabolick Music's Official Website">anabolicmusick.com</a></p>
+</div><div id="a190eaf64b5300a4f41b135bc0b3b445" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-footer" style="text-align: center;">Made with 💜 by <a data-_="Link" href="https://www.georgedreemer.com" target="_blank" title="George Dreemer Official Website">George Dreemer</a>.</p>
+</div><div id="a19ddf2e6dfe004054ad9daab8f0aaf3" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-footer" style="text-align: center;"><strong>© 2026 DREEMCORP</strong></p>
+</div></div></div><div id="a190eaf8eaab00d6119428b4a8217299" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a190eaf8eab000cf5465f1e330e31231" class="wb_element wb_element_picture" data-plugin="Picture" title="DREEMCORP George Dreemer Logo"><div class="wb_picture_wrap"><div class="wb-picture-wrapper"><a href="dreemcorp.com" title="DREEMCORP's Official Website"><img loading="lazy" alt="DREEMCORP George Dreemer Logo" src="gallery_gen/6742db93b980c975748be6c57a3e7e48_66x70_fit.png?ts=1777566122"></a></div></div></div></div></div><div id="wb_footer_c" class="wb_element" data-plugin="WB_Footer" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#footer, #footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div></div></div>{{hr_out}}</body>
+</html>
---
+++ public_html/ncsitebuilder/a188dd9cfa970054082e97d0725cb93a.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<script type="text/javascript">
+			</script>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars((isset($seoTitle) && $seoTitle !== "") ? $seoTitle : "Custom 404 page"); ?></title>
+	<base href="{{base_url}}" />
+	<?php echo isset($sitemapUrls) ? (generateCanonicalUrl($sitemapUrls)."\n") : ""; ?>	
+	
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+						<meta name="keywords" content="<?php echo htmlspecialchars((isset($seoKeywords) && $seoKeywords !== "") ? $seoKeywords : "Custom 404 page"); ?>" />
+			<meta name="robots" content="noindex" />
+		
+	<!-- Facebook Open Graph -->
+						<!-- Facebook Open Graph end -->
+
+		<meta name="generator" content="Website Builder" />
+			<script src="js/common-bundle.js?ts=20260430192201" type="text/javascript"></script>
+	<script src="js/a188dd9cfa970054082e97d0725cb93a-bundle.js?ts=20260430192201" type="text/javascript"></script>
+	<link href="css/common-bundle.css?ts=20260430192201" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,cyrillic-ext,latin,latin-ext,vietnamese" rel="stylesheet" type="text/css" />
+	<link href="css/a188dd9cfa970054082e97d0725cb93a-bundle.css?ts=20260430192201" rel="stylesheet" type="text/css" id="wb-page-stylesheet" />
+	<ga-code/><!-- HTML Meta Tags -->
+<title>CryptoPandemic Official Website</title>
+<meta name="description" content="Welcome to CryptoPandemic's Official Website!">
+<meta name="author" content="George Dreemer">
+<meta name="robots" content="index, follow">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://cryptopandemic.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="CryptoPandemic's Official Website">
+<meta property="og:description" content="Welcome to CryptoPandemic's Official Website!">
+<meta property="og:image" content="https://cryptopandemic.com/gallery_gen/e6752594e756a7de0a81a328e4fdf98a_fit.png">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="cryptopandemic.com">
+<meta property="twitter:url" content="https://cryptopandemic.com">
+<meta name="twitter:title" content="CryptoPandemic's Official Website">
+<meta name="twitter:description" content="Welcome to CryptoPandemic's Official Website!">
+<meta name="twitter:image" content="https://cryptopandemic.com/gallery_gen/e6752594e756a7de0a81a328e4fdf98a_fit.png"><link rel="shortcut icon" href="gallery/favicons/favicon.ico" type="image/x-icon">
+	<script type="text/javascript">
+	window.useTrailingSlashes = false;
+	window.disableRightClick = false;
+	window.currLang = 'en';
+</script>
+		
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+		<script type="text/javascript">
+		$(function () {
+<?php $wb_form_send_success = popSessionOrGlobalVar("wb_form_send_success"); ?>
+<?php if (($wb_form_send_state = popSessionOrGlobalVar("wb_form_send_state"))) { ?>
+	<?php if (($wb_form_popup_mode = popSessionOrGlobalVar("wb_form_popup_mode")) && (isset($wbPopupMode) && $wbPopupMode)) { ?>
+		if (window !== window.parent && window.parent.postMessage) {
+			var data = {
+				event: "wb_contact_form_sent",
+				data: {
+					state: "<?php echo str_replace('"', '\"', $wb_form_send_state); ?>",
+					type: "<?php echo $wb_form_send_success ? "success" : "danger"; ?>"
+				}
+			};
+			window.parent.postMessage(data, "<?php echo str_replace('"', '\"', popSessionOrGlobalVar("wb_target_origin")); ?>");
+		}
+	<?php $wb_form_send_success = false; $wb_form_send_state = null; $wb_form_popup_mode = false; ?>
+	<?php } else { ?>
+		wb_show_alert("<?php echo str_replace(array('"', "\r", "\n"), array('\"', "", "<br/>"), $wb_form_send_state); ?>", "<?php echo $wb_form_send_success ? "success" : "danger"; ?>");
+	<?php } ?>
+<?php } ?>
+});    </script>
+</head>
+
+
+<body class="site site-lang-en<?php if (isset($wbPopupMode) && $wbPopupMode) echo ' popup-mode'; ?> " <?php ?>><div id="wb_root" class="root wb-layout-vertical"><div class="wb_sbg"></div><div id="wb_header_a188dd9cfa970054082e97d0725cb93a" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"></div></div><div id="wb_main_a188dd9cfa970054082e97d0725cb93a" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="a189ad2f17c4006d4a8e4815dcdc5025" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-horizontal"><div id="a189ad2f17c6000a9899c4a0ea2b14f4" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h2 class="wb-stl-heading2"><span style="color:#000000;">404</span></h2>
+</div><div id="a189ad2f17cd00b76952516cd97db117" class="wb_element wb-elm-orient-vertical" data-plugin="Line"><div class="wb-elm-line"></div></div><div id="a189ad2f17d0003fe2c99bc80eeb0c51" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h2 class="wb-stl-heading2">Page not found</h2>
+</div></div></div></div></div><div id="wb_footer_a188dd9cfa970054082e97d0725cb93a" class="wb_element wb-layout-element" data-plugin="LayoutElement"><div class="wb_content wb-layout-vertical"><div id="wb_footer_c" class="wb_element" data-plugin="WB_Footer" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#footer, #footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div></div></div>{{hr_out}}</body>
+</html>
